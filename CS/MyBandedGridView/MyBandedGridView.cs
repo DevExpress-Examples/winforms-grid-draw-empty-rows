5a93feadae69a724121b66a839934cf289a32c16
DevExpressExampleBot <scmaintenance@devexpress.com>
2025-09-11 13:29:43 +0300

    Change example status to out of maintenance

--- CS/MyBandedGridView/MyBandedGridView.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CustomGrid {
-	public class MyBandedGridView : DevExpress.XtraGrid.Views.BandedGrid.BandedGridView {
-		public MyBandedGridView() : this(null) {}
-		public MyBandedGridView(DevExpress.XtraGrid.GridControl grid) : base(grid) {}
-		protected override string ViewName { get { return "MyBandedGridView"; } }
-	}
-}
--- CS/MyGridView/MyGridView.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CustomGrid {
-	public class MyGridView : DevExpress.XtraGrid.Views.Grid.GridView {
-		public MyGridView() : this(null) {}
-		public MyGridView(DevExpress.XtraGrid.GridControl grid) : base(grid) {}
-		protected override string ViewName { get { return "MyGridView"; } }
-	}
-}
